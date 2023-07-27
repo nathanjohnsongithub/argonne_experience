815c74a1fe686dcd69c6903cc32139d1a77f330a
nathanjohnsongithub <nathanjohnson482@gmail.com>
2023-07-27 14:42:23 -0500

    added comments and .txt file

--- mpi4py/mpi_three.py
+++ mpi4py/mpi_three.py
@@ -7,64 +7,82 @@ rank = comm.Get_rank()
 size = comm.Get_size()
 
 def recieve():
-    arr = comm.recv(None,0)   
-    for i in range(len(arr)):   
-        arr[i] = rank
+    # recieve the array from the master
+    arr = comm.recv(None,0)  
+    # change the arr into the value of the rank doing it 
+    arr = [rank for _ in arr]
     return arr 
 
 
 def split_number(number, divisor):
+    # get the quotient and the remainder
     quotient = number // divisor 
     remainder = number % divisor 
 
+    # make the parts array equal to the size of the divisor also known as the # of ranks
     parts = [0]*(divisor) 
 
     for i in range(divisor): 
+        # if there is still a remainder
         if(i < remainder):
+            # add the remainder
             parts[i] += 1    
+        # add on the last value to the current value plus the quotient
+        # e.g. number = 50, divisor = 4. parts[1] = 12 + 0 + 12.
         parts[i] += parts[i-1] + quotient 
     
     return parts
 
 def make_file():
     if(rank == 0):
-        f = open('myfile1.txt', 'w+')   # open file
+        # open the file
+        f = open('myfile1.txt', 'w+')   
 
+        # make the string and the list
         s = ''
+        lst = []
 
         for i in range(n):
             s += str(i) + '\n'
+            lst.append(i)
 
         f.write(s) 
-
-        f.seek(0)
-        lst = [int(x) for x in f.read().split()]
         f.close()
         
+        # call the split_number function to get an array with the distribution of the splice for each rank
         arr_with_distribution = split_number(n, size)
         
-        for i in range(0,size-1):  #loop through all the ranks and send there section of the array
-            arr_splice = lst[arr_with_distribution[i]: arr_with_distribution[i+1]] # [1250, 2500, 3750, 5000]
+        # loop through all the ranks and send there section of the array
+        for i in range(0,size-1):  
+            # section of the array to get sent
+            arr_splice = lst[arr_with_distribution[i]: arr_with_distribution[i+1]]
+            # send the section of the array
             comm.send(arr_splice, dest=i+1)    
             
-
+        # change the values of the array for the master
         for i in range(arr_with_distribution[0]):   
             lst[i] = rank
     else:
+        # send back the changed array to the master
         comm.send(recieve(), dest=0) #all of the ranks send there converted array back to the master
 
     if(rank == 0):
+        # the master returns the arr_with_distribution and the lst
         return arr_with_distribution, lst
     
 def upload(lst):
+    # create the changed file
     fchanged = open('mychangedfile1.txt', 'w')
         
     for i in range(n):
+        # write the values to the new file
         fchanged.write(str(lst[i]) + '\n')
     
+    # close the file
     fchanged.close()
 
 def recieve_and_populate(arr_with_distribution, lst):
+    # loop through and recieve the array and put it into the lst
     for i in range(0,size-1):
         lst[arr_with_distribution[i]: arr_with_distribution[i+1]] = comm.recv(None, i+1) #then loops through and recieves each section of the converted array 
 
