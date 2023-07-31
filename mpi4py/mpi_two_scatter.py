7a95e65dda2e127d1e7267c5c0b5ff4bc7af2b80
nathanjohnsongithub <nathanjohnson482@gmail.com>
2023-07-31 12:03:00 -0500

    Modified mpi_two_scatter

--- mpi4py/mpi_two_scatter.py
+++ mpi4py/mpi_two_scatter.py
@@ -1,68 +1,69 @@
 from mpi4py import MPI
+import numpy as np
+import math
 
+# Initialize MPI and get the rank of each process and the # of ranks (size)
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 size = comm.Get_size()
 
-#####################ENTER SIZE OF ARRAY#########################
-n = 16
-#################################################################    
+##################### ENTER SIZE OF ARRAY #########################
+n = 83
+###################################################################    
 
-def recieve():
-    arr = comm.recv(None,0)    # each rank recieves there portion of the array
-    print(rank, 'recieved', arr)
-    for i in range(len(arr)):  # they then convert the array to whatever you want
-        arr[i] += 100*rank
-    
-    return arr # and return it 
+def make_matrix(arr, rows):
+    # get the size of the columns rounding up from the size of the array and the # or ranks
+    cols = math.ceil(len(arr) / size)
 
- # make an empty array of size 
-def split_list(lst, n):
+    # Add the extra Null values so its an equal matrix
+    arr += [None] * (rows - len(arr) % rows)
 
-    length = len(lst)
-    split_size = length // n  # Calculate the split size
-    print(split_size)
-    remainder = length%n
-    forgotten = [0]*remainder
-    j = 0
-    for i in range(length-remainder,length):
-        forgotten[j] = lst[i]
-        if(j < remainder):
-            j += 1
-        else:
-            break
+    # create the matrix and populate it with values fror the array 
+    matrix = [[arr[i * cols + j] for j in range(cols)] for i in range(rows)]
 
-    result = [lst[i:i + split_size] for i in range(0, n*split_size, split_size)]
-    
-    return result 
+    return matrix
 
+def flatten_matrix(matrix):
+    # create a numpy matrix from the matrix and flatten it
+    temp_matrix = np.array(matrix).flatten()
+    # return the list version of the numpy matrix
+    return temp_matrix.tolist()
 
-if(rank == 0): #if the process is the master
-    arr = [0] * n
-    print('empty arr',arr) #  print empty array
-    for i in range(n):     #  populate array only for master
+arr = [0] * n # make an empty array of size n
+
+# if the process is the master
+if(rank == 0): 
+    #  populate array only for master
+    for i in range(n):     
         arr[i] = i
     
-    #arr = split_list(arr, size)
-    print(arr)
+    # Get the matrix
+    matrix = make_matrix(arr, size)
+    # Print it out
+    print("Matrix we will be scattering\n", matrix, "\n")
 else:
-    arr = None
+    # Make the matrix equal to nothing for all other ranks (temporary)
+    matrix = None
+
+# Scatter the array to all the ranks equally
+matrix = comm.scatter(sendobj=matrix, root=0)
+
+# Each rank prints out there section they recieved
+print(f"rank {rank}: has this section: {matrix}")
+
+# Each rank adds 100 times there rank to the value of the matrix
+matrix = [value + 100 * rank if value is not None else None for value in matrix]
+
+# gather the now converted matrix 
+converted_matrix = comm.gather(matrix, 0)
+
+# flatten the matrix
+# NOTE Could just have everything as a numpy array as theyre better but I didnt want to change the list comprehension so we've got this
+converted_matrix = flatten_matrix(converted_matrix)
+
+# Master prints out the converted matrix
+if rank == 0:
+    print("\nConverted Matrix\n", converted_matrix)
     
-    #arr_with_distribution = split_number(n, size) #make a spliced array of the number of ranks + one for a zero at the start using the split number function
-
-arr = comm.scatter(sendobj=arr, root=0)
-
-print('I am rank',rank, 'i got', arr)
-
-'''
-comm.send(recieve(), dest=0) #all of the ranks send there converted array back to the master
-
-if(rank == 0):
-    print('\noriginal ', arr) #master prints the original array
-    for i in range(0,size):
-        arr[arr_with_distribution[i]: arr_with_distribution[i+1]] = comm.recv(None, i) #then loops through and recieves each section of the converted array 
-
-    print('\nconverted\n',arr) #print converted array
 
 MPI.Finalize() # close MPI
-'''
